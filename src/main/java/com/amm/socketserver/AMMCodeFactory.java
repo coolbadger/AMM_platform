8dfaac15e821df34f45edb83ff6b9d55175118e7
lekoxnfx <lekoxnfx@qq.com>
2016-07-17 22:52:49 +0800

    调整Socket Server 编码

--- src/main/java/com/amm/socketserver/AMMCodeFactory.java
+++ src/main/java/com/amm/socketserver/AMMCodeFactory.java
@@ -4,16 +4,23 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFactory;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolEncoder;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.codec.textline.TextLineDecoder;
+import org.apache.mina.filter.codec.textline.TextLineEncoder;
+
+import java.nio.charset.Charset;
 
 /**
  * Created by liuminhang on 16/7/16.
  */
 public class AMMCodeFactory implements ProtocolCodecFactory {
     public ProtocolEncoder getEncoder(IoSession ioSession) throws Exception {
-        return null;
+        TextLineEncoder textLineEncoder = new TextLineEncoder(Charset.forName("US-ASCII"));
+        return textLineEncoder;
     }
 
     public ProtocolDecoder getDecoder(IoSession ioSession) throws Exception {
-        return null;
+        TextLineDecoder textLineDecoder = new TextLineDecoder(Charset.forName("US-ASCII"));
+        return textLineDecoder;
     }
 }
---
+++ src/main/java/com/amm/socketserver/AMMCodeFactory2.java
@@ -0,0 +1,13 @@
+package com.amm.socketserver;
+
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+
+/**
+ * Created by liuminhang on 16/7/17.
+ */
+public class AMMCodeFactory2 extends TextLineCodecFactory {
+    AMMCodeFactory2(){
+        super();
+
+    }
+}
