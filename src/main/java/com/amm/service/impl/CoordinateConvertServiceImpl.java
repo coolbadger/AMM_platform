7342b7c6bc92ca150c42b86d65a58f81ec2cdc0b
luyan19910701 <18358109606@163.com>
2017-03-15 16:17:01 +0800

    测试提交

---
+++ src/main/java/com/amm/service/impl/CoordinateConvertServiceImpl.java
@@ -0,0 +1,87 @@
+package com.amm.service.impl;
+
+import com.amm.entity.GpsRecordEntity;
+import com.amm.gps.GpsResultDetail;
+import com.amm.gps.WebRequest;
+import com.amm.repository.GpsRecordRepository;
+import com.amm.service.CoordinateConvertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by ThinkPad on 2017-03-09.
+ */
+@Component("CoordinateConvertService")
+@Scope("prototype")
+public class CoordinateConvertServiceImpl implements CoordinateConvertService {
+
+    @Autowired
+    GpsRecordRepository gpsRecordRepository;
+
+    @PostConstruct
+    public void convert() {
+
+        List<GpsRecordEntity> list = gpsRecordRepository.getUnConvert();
+
+        GpsRecordEntity gpsRecordEntity;
+        StringBuffer buffer = new StringBuffer();
+        int j = 0;
+
+        for (int i = 0; i<list.size();i++){
+            gpsRecordEntity = list.get(i);
+            if (gpsRecordEntity.getLng()==null||gpsRecordEntity.getLat()==null
+                    ||"".equals(gpsRecordEntity.getLng())||"".equals(gpsRecordEntity.getLat())
+                    ||"0".equals(gpsRecordEntity.getLng())||"0".equals(gpsRecordEntity.getLat())){
+                gpsRecordEntity.setFlag("2");//Gps数据不合法
+                gpsRecordRepository.saveAndFlush(gpsRecordEntity);
+                list.remove(i);//将不合法数据踢出list
+            }
+        }
+
+       while (list.size()>0) {
+
+        while (j<90){
+
+            try{
+                buffer.append(list.get(j).getLng());
+                buffer.append(",");
+                buffer.append(list.get(j).getLat());
+                buffer.append(";");
+                j++;
+            }catch (IndexOutOfBoundsException e){
+                e.printStackTrace();
+                break;
+            }
+
+        }
+        j=0;
+
+        String newCoordStr = buffer.deleteCharAt(buffer.length()-1).toString();//百度api允许请求格式为{"xxxx,xxxx;xxxx,xxxx"}
+        List<GpsResultDetail> gpsResultDetails = WebRequest.getGpsFixed(newCoordStr);//请求百度api批量转换坐标
+
+        for (int i = 0;i<gpsResultDetails.size();i++){
+            list.get(i).setLngFixed(gpsResultDetails.get(i).getLngFixed());
+            list.get(i).setLatFixed(gpsResultDetails.get(i).getLatFixed());
+            list.get(i).setFlag("1");
+            gpsRecordRepository.saveAndFlush(list.get(i));//转换完成 更新数据
+        }
+
+        Iterator<GpsRecordEntity> it = list.iterator();
+           while (it.hasNext()){
+               String flag = it.next().getFlag();
+               if ("1".equals(flag)||"2".equals(flag)){
+                   it.remove();
+               }
+           }
+
+        buffer = buffer.delete(0,buffer.length());
+    }
+
+    }
+
+}
