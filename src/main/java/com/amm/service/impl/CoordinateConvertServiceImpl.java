bf3a3550ad64eb1487cd24746ef1af9280a1832e
luyan19910701 <18358109606@163.com>
2017-03-30 09:30:42 +0800

    list.remove不能用来删除数据

--- src/main/java/com/amm/service/impl/CoordinateConvertServiceImpl.java
+++ src/main/java/com/amm/service/impl/CoordinateConvertServiceImpl.java
@@ -39,7 +39,7 @@ public class CoordinateConvertServiceImpl implements CoordinateConvertService {
                     ||"0".equals(gpsRecordEntity.getLng())||"0".equals(gpsRecordEntity.getLat())){
                 gpsRecordEntity.setFlag("2");//Gps数据不合法
                 gpsRecordRepository.saveAndFlush(gpsRecordEntity);
-                list.remove(i);//将不合法数据踢出list
+//                list.remove(i);//将不合法数据踢出list
             }
         }
 
@@ -62,26 +62,30 @@ public class CoordinateConvertServiceImpl implements CoordinateConvertService {
                 buffer.append(list.get(j).getLat());
                 buffer.append(";");
                 j++;
-            }catch (Exception e){
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("最后一条数据");
+                break;
+            } catch (Exception e){
                 e.printStackTrace();
                 break;
             }
 
         }
         j=0;
-
-        String newCoordStr = buffer.deleteCharAt(buffer.length()-1).toString();//百度api允许请求格式为{"xxxx,xxxx;xxxx,xxxx"}
-        List<GpsResultDetail> gpsResultDetails = WebRequest.getGpsFixed(newCoordStr);//请求百度api批量转换坐标
-
-        //逐条更新结果
-        for (int i = 0;i<gpsResultDetails.size();i++){
-            list.get(i).setLngFixed(gpsResultDetails.get(i).getLngFixed());
-            list.get(i).setLatFixed(gpsResultDetails.get(i).getLatFixed());
-            list.get(i).setFlag("1");
-            gpsRecordRepository.saveAndFlush(list.get(i));//转换完成 更新数据
+           String newCoordStr;
+        try{
+            newCoordStr  = buffer.deleteCharAt(buffer.length()-1).toString();//百度api允许请求格式为{"xxxx,xxxx;xxxx,xxxx"}
+            List<GpsResultDetail> gpsResultDetails = WebRequest.getGpsFixed(newCoordStr);//请求百度api批量转换坐标
+            //逐条更新结果
+            for (int i = 0;i<gpsResultDetails.size();i++){
+                list.get(i).setLngFixed(gpsResultDetails.get(i).getLngFixed());
+                list.get(i).setLatFixed(gpsResultDetails.get(i).getLatFixed());
+                list.get(i).setFlag("1");
+                gpsRecordRepository.saveAndFlush(list.get(i));//转换完成 更新数据
+            }
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("字符串处理");
         }
-
-
         buffer = buffer.delete(0,buffer.length());
     }
 
