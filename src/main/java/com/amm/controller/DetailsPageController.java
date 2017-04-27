c45db47fa3101ed54c066a04b2938a30c2850abe
杨思明 <1057842101@qq.com>
2017-04-27 15:27:34 +0800

    合作社分析页面  保留两位小数

--- src/main/java/com/amm/controller/DetailsPageController.java
+++ src/main/java/com/amm/controller/DetailsPageController.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,11 +74,15 @@ public class DetailsPageController extends BaseController{
 
                     }
                 }
+                DecimalFormat df = new DecimalFormat("0.00");
+                String WorkTime=df.format(tempWorkTime);
+                String DrivingArea=df.format(tempDrivingArea);
+                String WorkArea=df.format(tempWorkArea);
                 //农机信息
                 detailsPage.setMachSize(tempMachSize);
-                detailsPage.setAllWorkTime(Float.toString(tempWorkTime));
-                detailsPage.setAllDrivingArea(Float.toString(tempDrivingArea));
-                detailsPage.setAllWorkArea(Float.toString(tempWorkArea));
+                detailsPage.setAllWorkTime(WorkTime);
+                detailsPage.setAllDrivingArea(DrivingArea);
+                detailsPage.setAllWorkArea(WorkArea);
                 temp.add(detailsPage);
             }
         return temp;
